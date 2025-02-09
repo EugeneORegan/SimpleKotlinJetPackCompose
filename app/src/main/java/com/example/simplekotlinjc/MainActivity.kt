dc55dc56608e1246fdfd20d816b20e90d991f080
EugeneOReganWiley <eoregan@wiley.com>
2025-02-09 15:52:26 +0000

    Update

--- app/src/main/java/com/example/simplekotlinjc/MainActivity.kt
+++ app/src/main/java/com/example/simplekotlinjc/MainActivity.kt
@@ -8,11 +8,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
@@ -26,6 +31,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.simplekotlinjc.ui.theme.SimpleKotlinJCTheme
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.simplekotlinjc.ComposableTwo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +45,10 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding))
-
-
+                    Spacer(modifier = Modifier.height(20.dp))
+                  //  ComposableTwo( modifier = Modifier.padding(200.dp))
                 }
+
             }
         }
     }
@@ -52,28 +61,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column {
         Modifier
             .fillMaxWidth()
-            .align(alignment = Alignment.End)
+            .align(alignment = Alignment.End )
+            .padding(horizontal  = 200.dp, vertical = 300.dp)
 
 
         Text(
             text = "Hello $name!",
-           modifier = modifier
+            color = Color.Red,
+            modifier = Modifier.background(Color.Green)
 
         )
         Button(onClick = {
             x++
          println("Hello  $x")
         }) {
-            Modifier
-                .fillMaxWidth()
 
-
-                Text("Click Me")
+            Text("Click Me")
             }
         }
     }
 
+@Composable
+fun ComposableTwo(modifier: Modifier = Modifier)
+{
 
+    Column(
+        modifier = Modifier.background(Color.Blue),
+
+    ) {
+        Text("This is ")
+        Button(onClick = {}) {
+            Text("Press Me")
+        }
+    }
+
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
